1565541b70c027b738e374b97ec82d47ca96609d
RISHAB <rkmr039@gmail.com>
2019-05-17 15:01:58 +0530

    creating threads using anonymous class

---
+++ Multithreading/src/demo3/App2.java
@@ -0,0 +1,22 @@
+package demo3;
+
+public class App2 {
+	public static void main(String []args)
+	{
+		new Thread() {
+			public void run()
+			{
+				for(int i=0; i<10; i++) {
+					System.out.println("T1 : "+i );
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}.start();
+	}
+
+}
