e2499f19ca3944a1c2851f2a471ac1518eb95532
RISHAB <rkmr039@gmail.com>
2019-05-20 19:02:45 +0530

    synchronization with accounts in trouble

---
+++ Multithreading/src/demo5/ThreadDemo.java
@@ -0,0 +1,65 @@
+package demo5;
+import java.util.Scanner;
+
+class Account {
+	
+	private int bal;
+	
+	public Account(int bal)
+	{
+		this.bal = bal;
+	}
+	public boolean isSufficientBalance(int w) {
+		if(bal > w) return true;
+		else return false;
+	}
+	
+	public void withdraw(int amt) {
+		bal = bal-amt;
+		
+		System.out.println("Withdraw money is : "+ amt);
+		System.out.println("Current Balance : "+bal);
+	}
+}
+
+class Customer implements Runnable{
+	
+	private Account account;
+	private String name;
+	public Customer(Account account, String n) {
+		this.account = account;
+		this.name = n;
+	}
+ 
+	@Override
+	public void run() {
+		Scanner kb = new Scanner(System.in);
+		System.out.println(this.name + " Enter amount to withdraw : ");
+		int amt = kb.nextInt();
+		
+		if(account.isSufficientBalance(amt)) {
+			System.out.println(this.name );
+			account.withdraw(amt);
+		}
+		else {
+			System.out.println("Insufficient Balance ");
+		}
+		
+	}
+}
+public class ThreadDemo {
+	
+	public static void main(String []args) {
+	
+		Account a1 = new Account(1000);
+		Customer c1 = new Customer(a1,"Raj"),c2 = new Customer(a1,"Sunaina");
+		
+		
+		Thread t1 = new Thread(c1);
+		Thread t2 = new Thread(c2);
+		
+		t1.start();
+		t2.start();
+		
+	}
+}
