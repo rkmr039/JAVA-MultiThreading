5e3e5242ae1a7a7e483cd567f6bdb89cafdbc3ff
RISHAB <rkmr039@gmail.com>
2019-05-28 21:43:23 +0530

    Synchronization using ReentrantLock and Condition Class

--- Multithreading/src/ReentrantLocks/Runner.java
+++ Multithreading/src/ReentrantLocks/Runner.java
@@ -1,5 +1,7 @@
 package ReentrantLocks;
 
+import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -13,8 +15,14 @@ public class Runner {
 	
 
 	private int count = 0;
-	
 	private Lock lock = new ReentrantLock();
+	private Condition cond = lock.newCondition();
+	
+	/*
+	 * A java.util.concurrent.locks.Condition interface provides a thread ability to suspend its execution,
+	 * until the given condition is true. 
+	 * A Condition object is necessarily bound to a Lock and to be obtained using the newCondition() method.
+	 */
 	private void increment() {
 		for(int i=0; i<10000; i++) {
 			count++;
@@ -22,13 +30,33 @@ public class Runner {
 	}
 	public void firstThread() throws InterruptedException{
 		lock.lock();
-		increment();
-		lock.unlock();
+		System.out.println("Waiting....");
+		cond.await();
+		
+		System.out.println("Woken up....");
+		
+		try {
+			increment();
+		}finally{
+			lock.unlock();	
+		}
+		
 	}
 	public void secondThread() throws InterruptedException{
+		
+		Thread.sleep(1000);
 		lock.lock();
-		increment();
-		lock.unlock();
+		
+		System.out.println("Press the Return key....");
+		new Scanner(System.in).nextLine();
+		System.out.println("Got Return Key.....");
+		cond.signal();
+		
+		try {
+			increment();
+		}finally{
+			lock.unlock();	
+		}
 	}
 	public void finished() {
 		System.out.println("Count is : "+ count);
