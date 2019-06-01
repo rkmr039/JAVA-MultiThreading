153774bdb63ba2ea07dfcc82ec3c57898eb46c22
RISHAB <rkmr039@gmail.com>
2019-06-01 19:49:30 +0530

    Counting Semaphore Demo

--- Multithreading/src/semaphores/Connection.java
+++ Multithreading/src/semaphores/Connection.java
@@ -6,7 +6,7 @@ public class Connection {
 	
 	private static Connection instance = new Connection();
 	
-	private Semaphore sem = new Semaphore(10);
+	private Semaphore sem = new Semaphore(10, true);
 	
 	private int connections = 0;
 	
@@ -14,14 +14,23 @@ public class Connection {
 	public static Connection getInstance() {
 		return instance;
 	}
+	
 	public void connect() {
-		
-	try {
-		sem.acquire();
-	} catch (InterruptedException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
+		try {
+			sem.acquire();
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			doConnect();
+		}finally {
+			sem.release();
+		}
 	}
+	public void doConnect() {
+		
+	
 	synchronized (this) {
 		connections++;
 		System.out.println("Current Connections : "+ connections);
@@ -33,6 +42,6 @@ public class Connection {
 	}
 	synchronized (this) {
 		connections--;
-	} sem.release();
+	} 
 	}	
 }
